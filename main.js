2cb6cae73f88c2fb1087f95303b9da07e5c18413
Bruna Lekich Louro <brunalekich@gmail.com>
2022-12-15 14:06:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+//Variável do canvas:
+
+
+//Posição x e y do jogador:
+playerX = 10;
+playerY = 10;
+
+//Altura e largura da img do bloco:
+
+
+
+//Variáveis de objeto e bloco:
+
+
+
+function playerUpdate(){
+
+}
+
+function newImage(getImage){
+
+}
+
+//Escutador de eventos:
+
+
+//Funções de movimento:
+function myKeyDown(e){
+
+}
+
+function up(){
+
+}
+function down(){
+
+}
+function left(){
+
+}
+function right(){
+
+}
